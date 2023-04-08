5e3cb96c418c02ec24221aa1332153192ac51dea
Olayuwa Timilehin Ebenezer <timileyinolayuwa@gmail.com>
2023-04-08 17:38:42 +0100

    todo app created and completed

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # JS Projects
- This Repos consits of my JS projects
+
+This Repos consits of my JS projects.
+Projects that will be created includes
+1️⃣ ✅ todo list app for managing tasks
+2️⃣ calculator app that performs basic arithemetic
+3️⃣ A quiz app that allows users to take quizes on various topics and anounce their scores at the end.
+4️⃣ A random quote generator that generates random quotes or affirmations
+5️⃣ A Weather App that displays the weather inforation for a given location
+6️⃣ An E commerce website
+
+Each of this projects will be further updated as I become advanced.
--- todo/todo.js
+++ todo/todo.js
@@ -1,8 +1,10 @@
+
 const night = document.querySelector(".night");
 const light = document.querySelector(".light");
 const body = document.querySelector("body");
 const headgear = document.querySelector(".headgear");
-const plus = document.querySelector(".plus");
+
+// the events
 
 night.addEventListener("click", () =>{
     night.style.display = "none";
@@ -16,16 +18,52 @@ light.addEventListener("click", () => {
     body.style.backgroundColor = "#f9f9f9";
     night.style.display = "#flex";
 });
+
+// add a todo event
+const todo_main = document.querySelector(".todo_main");
+const input_main = document.querySelector(".input_main");
+const text = document.querySelector("input");
+const plus = document.querySelector(".plus");
+const main = document.querySelector("main");
+let counter = 0;
 plus.addEventListener("click", () => {
-    let input = document.querySelector("input");
-    let main = document.querySelector("main");
-    let div = document.createElement("div");
-    div.setAttribute("class", "list_container");
-    let textField = document.createElement("p");
-    textField.setAttribute("class", "todoText");
-    textField.innerHTML = input.value;
-    div.appendChild(textField);
-    main.appendChild(div);
-    main.style.display = "block";
-    textField.style.color = "#f9f9f9"
+    let textDiv = document.createElement("div");
+    textDiv.setAttribute("class", "textrow");
+    let sn = document.createElement("p");
+    sn.classList.add("eachCounts");
+    sn.classList.add("plus")
+    counter +=1
+    sn.innerHTML= counter;
+    textDiv.appendChild(sn);
+
+    let textInput = document.createElement("p");
+    textInput.classList.add("myListText");
+    textInput.innerHTML = text.value;
+    textInput.style.textAlign = "left"
+    textDiv.appendChild(textInput);
+
+    let del = document.createElement("button");
+    del.classList.add("plus");
+    del.classList.add("plusbody")
+    del.innerHTML = "&times;";
+    textDiv.appendChild(del);
+    
+    main.appendChild(textDiv)
+    // clears the input box.
+    
+    // main container does not display if the text field is empty
+    if (text.value === ""){
+        textDiv.style.display = "none";
+        alert("The text box cannot be empty")
+        counter-=1;
+        return;
+    }else{
+        main.style.display = "block"
+    }
+    text.value = ""
+    main.style.display = "block"
+    del.addEventListener("click", () =>{
+        deltextDiv = main.removeChild(textDiv);
+        counter -=1            
+        });
 });
